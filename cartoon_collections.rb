d6dad8eb989c099d8f9e5f5d87477fc942fea1c5
support <support@kmcintosh-6782.remax.local>
2023-05-02 19:46:48 -0600

    commit tree 34e88803c8834836137f8975544b16d430a76f91

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
+  sorted = array.map.with_index(1) {|str, index|
+    "#{index}. #{str}"}
+    puts sorted
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)# code an argument here
+  capitalized = planeteer_calls.map {|str| 
+  str[0].upcase + str.slice(1..) + "!"}# Your code here
+  capitalized
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.any? {|str| str.length > 4}# Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)# code an argument here
+  has_cheese = array.find {|str| str.include?("cheddar" || "gouda" || "camembert")}# the array below is here to help
+  # cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+# find_the_cheese(["che", "afdsa", "afafafa"])
